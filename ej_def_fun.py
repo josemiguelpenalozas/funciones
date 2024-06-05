b10a83f7568249a05f613f7e9386ed1c3b9970b4
jose miguel <jo.penalozas@duocuc.cl>
2024-06-05 10:38:04 -0400

    primera version de mis archivos

---
+++ ej_def_fun.py
@@ -0,0 +1,5 @@
+import func as fn
+primer=int(input("ingrese primer numero: "))
+segundo=int(input("ingrese segundo numero: "))
+
+print(fn.promedio_2(primer,segundo))
---
+++ func.py
@@ -0,0 +1,4 @@
+def promedio_2 (num1,num2):
+    promedio=num1+num2
+    promedio=promedio/2
+    return promedio
